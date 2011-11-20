a90c342d514b19b5960016817c8b4573b6c36d68
unknown <yz8ra@Chromium.(none)>
2011-11-20 12:14:19 -0500

    changed remote server to chromium.cs.virginia.edu:12345

--- content/Selenium/start_server.cmd
+++ content/Selenium/start_server.cmd
@@ -1,1 +1,1 @@
-java -jar %Desktop%\selenium-server-standalone-2.13.0.jar -firefoxProfileTemplate "C:\Users\Yuchen\AppData\Roaming\Mozilla\Firefox\Profiles\9vqwolbc.Dev"
+java -jar %Desktop%\selenium-server-standalone-2.13.0.jar -firefoxProfileTemplate "C:\Users\yz8ra\AppData\Roaming\Mozilla\Firefox\Profiles\d119r01a.default" -port 12345
