60991bce27814ba72b473f94433cc92410f19281
unknown <yz8ra@Chromium.(none)>
2011-12-09 12:54:14 -0500

    added nytimeshome.rb

--- content/Policy/PolicyGen_simpl/conf.rb
+++ content/Policy/PolicyGen_simpl/conf.rb
@@ -5,7 +5,7 @@
 #------------------------------------------------------
 HostDomain = "nytimescom"
 HostURL = "httpwwwnytimescom20111205technologyxboxlivechallengesthecablebo"
-P_inst = 0.05								#instrumentation frequency: when Alldomain is set to true, this maybe overriden to a higher value.
+P_inst = 0.07								#instrumentation frequency: when Alldomain is set to true, this maybe overriden to a higher value.
 Thres = 0.1									#allowed maximum false positive
 Alldomain = true							#allow the model builder to first scan all records and record all files that contain a previously unrecorded domain. Those files will be automatically considered in training phase.
 MinRep = 5									#Only effective when Alldomain is set to true. Instead of having one representative for each domain, a minimum number of representatives are required for each domain.
