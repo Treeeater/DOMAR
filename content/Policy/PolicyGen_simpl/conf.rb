8722d875eb654afe6b8e1b7602b03f951caeffe3
unknown <yz8ra@Chromium.(none)>
2012-01-21 14:55:20 -0500

    sync

--- content/Policy/PolicyGen_simpl/conf.rb
+++ content/Policy/PolicyGen_simpl/conf.rb
@@ -4,8 +4,9 @@
 #HostURL = "httpwwwyelpcomfairfaxva"
 #HostURL = "httpwwwyelpcomuserdetails"
 #------------------------------------------------------
-#HostDomain = "nytimescom"
-#HostURL = "httpwwwnytimescom_3"
+HostDomain = "nytimescom"
+HostURL = "httpwwwnytimescompagestodayspaperindexhtml"
+#HostURL = "httpwwwnytimescom"
 #HostURL = "httpwwwnytimescom20111205technologyxboxlivechallengesthecablebo"
 #HostURL = "httpthecaucusblogsnytimescom20120109daleytostepdownwhitehouseof"
 #HostDomain = "techcrunchcom"
@@ -17,8 +18,8 @@
 #HostURL = "httpslashdotorg"
 #HostDomain = "wsjcom"
 #HostURL = "httpblogswsjcomdeals20120109freshlowforzyngashares"
-HostDomain = "neweggcom"
-HostURL = "httpwwwneweggcomProductProductaspx"
+#HostDomain = "neweggcom"
+#HostURL = "httpwwwneweggcomProductProductaspx"
 P_inst = 0.05								#instrumentation frequency: when Alldomain is set to true, this maybe overriden to a higher value.
 Thres = 0.1									#allowed maximum false positive
 Alldomain = false							#allow the model builder to first scan all records and record all files that contain a previously unrecorded domain. Those files will be automatically considered in training phase.  *After discussing with Dave this option should be set to false all the time.*
@@ -27,4 +28,4 @@ MinRep = 5									#Only effective when Alldomain is set to true. Instead of hav
 Running_times = 1							#how many times we are going to run the whole program
 StrictModelThreshold = 0.01					#the maximum accepted FP threshold per-page per-TLD.
 RelaxedModelThreshold = 4					#the maximum accepted distance of indices to deem two records as the same
-RelaxedModeEnabled = false					#to enable relaxed mode
+RelaxedModeEnabled = true					#to enable relaxed mode
